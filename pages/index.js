9206298ad3a504a0d01ff49d927bcb48597182bb
Anna Akimova <35057470+AnnaAkimova@users.noreply.github.com>
2022-04-14 13:16:55 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router'
-import { NextResponse, NextRequest } from 'next/server'
+
 
 export default function Home() {
-   return NextResponse.redirect('/hello-nextjs')
+   
 }
