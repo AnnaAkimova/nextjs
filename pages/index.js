876ca447ad9c629f960a15a58e60df9a5cc85990
Anna Akimova <35057470+AnnaAkimova@users.noreply.github.com>
2022-04-14 13:10:27 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,5 @@ import { useRouter } from 'next/router'
 
 export default function Home() {
    const router = useRouter()
-      const CustomPanel = [
-           {href: '/users', text: 'All Lists user'},
-           {href: '/about', text: 'About'}
-      ]
-
-    useEffect(() => {}, [CustomPanel]) // All the magic is here
-
-    return (
-       <div>
-          {CustomPanel.map((item, inx) => (
-               <a key={inx} onClick={() => router.push(`${item.href}`)}>{item.text}</a>
-          ))}
-       </div>
-    )
+   router.replace('/home')
 }
