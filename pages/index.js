f72296e1edb586dfac8d62a5e57a9f3048e652d1
Anna Akimova <35057470+AnnaAkimova@users.noreply.github.com>
2022-04-14 12:48:53 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,5 +5,5 @@ import { useRouter } from 'next/router';
 
 export default function Home() {
  const router = useRouter();
-  router.replace('https://www.google.com/')
+  router.replace('https://www.google.com/');
 }
